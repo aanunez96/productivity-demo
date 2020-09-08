59dc38f3ba531c34871b2266abb3e23d665ad983
adrian alberto nunez capote <aanunez@estudiantes.uci.cu>
2020-09-08 10:19:47 -0400

    implement Header and sideBar as Navigator

---
+++ app/src/components/themes/Navigator.js
@@ -0,0 +1,183 @@
+import React, {useContext} from 'react';
+import clsx from "clsx";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar/AppBar";
+import {makeStyles} from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer/Drawer";
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import { mainListItems, secondaryListItems } from './listItems';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+import Menu from "@material-ui/core/Menu/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import {Context} from "../../utils/Store";
+import {accountsClient} from "../../utils/accounts-js";
+import {useHistory, Link} from "react-router-dom";
+
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    toolbar: {
+        paddingRight: 24, // keep right padding when drawer closed
+    },
+    toolbarIcon: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 36,
+    },
+    title: {
+        flexGrow: 1,
+    },
+    drawerPaper: {
+        position: 'relative',
+        whiteSpace: 'nowrap',
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerPaperClose: {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9),
+        },
+    },
+    menuButtonHidden: {
+        display: 'none',
+    },
+}));
+
+export default function Navigator() {
+    const classes = useStyles();
+    const [state, dispatch] = useContext(Context);
+    const user = state;
+    const history = useHistory();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [redirect, setRedirect] = React.useState(false);
+    const [openDrawer, setOpenDrawer] = React.useState(false);
+
+    const logout = async () => {
+        try {
+            await accountsClient.logout();
+            setRedirect(true);
+            dispatch({
+                type: 'LOGOUT'
+            })
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    if (redirect) {
+        setRedirect(false);
+        history.push('/login');
+        setAnchorEl(null);
+    }
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleDrawerOpen = () => {
+        setOpenDrawer(true);
+    };
+    const handleDrawerClose = () => {
+        setOpenDrawer(false);
+    };
+    return(
+       <>
+           <AppBar position="absolute" className={clsx(classes.appBar, openDrawer && classes.appBarShift)}>
+               <Toolbar className={classes.toolbar}>
+                   <IconButton
+                       edge="start"
+                       color="inherit"
+                       aria-label="open drawer"
+                       onClick={handleDrawerOpen}
+                       className={clsx(classes.menuButton, openDrawer && classes.menuButtonHidden)}
+                   >
+                       <MenuIcon />
+                   </IconButton>
+                   <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                       Dashboard
+                   </Typography>
+                   <Button component={Link} to={"/create-ad"} size="small">Add Task</Button>
+                   {
+                       (user) ?
+                           <div>
+                               <IconButton aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}
+                                           color="inherit" className={classes.iconButtonAvatar}>
+                                   <Avatar alt="My Avatar"/>
+                               </IconButton>
+                               <Menu
+                                   id="simple-menu"
+                                   anchorEl={anchorEl}
+                                   keepMounted
+                                   open={Boolean(anchorEl)}
+                                   onClose={handleClose}
+                               >
+                                   <MenuItem component={Link} to="/edit-profile" onClick={handleClose}>Profile</MenuItem>
+                                   <MenuItem onClick={logout}>Logout</MenuItem>
+                               </Menu>
+                           </div>
+                           :
+                           <Button component={Link} to={"/login"} variant="outlined" size="small">
+                               Sign up
+                           </Button>
+                   }
+               </Toolbar>
+           </AppBar>
+           <Drawer
+               variant="permanent"
+               classes={{
+                   paper: clsx(classes.drawerPaper, !openDrawer && classes.drawerPaperClose),
+               }}
+               open={openDrawer}
+           >
+               <div className={classes.toolbarIcon}>
+                   <IconButton onClick={handleDrawerClose}>
+                       <ChevronLeftIcon />
+                   </IconButton>
+               </div>
+               <Divider />
+               <List>{mainListItems}</List>
+               <Divider />
+               <List>{secondaryListItems}</List>
+           </Drawer>
+       </>
+    );
+}
