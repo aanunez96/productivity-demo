007d9609f7ac09b355303a666ddcf555c15ff1ee
adrian alberto nunez capote <aanunez@estudiantes.uci.cu>
2020-09-08 09:59:03 -0400

    define router with react-router-dom

--- app/src/dashboard.js
+++ app/src/dashboard.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from "react-router-dom";
+import {makeStyles} from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Link from '@material-ui/core/Link';
 import Navigator from './components/themes/Navigator';
-import Chart from './components/task/Chart';
-import Orders from './components/task/TableTask';
+import Home from './screens/home/Home';
+import SingIn from "./screens/authentication/SignIn";
+import SingUp from "./screens/authentication/SignUp";
+import Error from "./screens/Error";
 
 function Copyright() {
     return (
@@ -26,65 +30,52 @@ function Copyright() {
 }
 
 const useStyles = makeStyles((theme) => ({
-    root: {
+    app: {
+        flex: 1,
         display: 'flex',
+        minHeight: '100vh'
     },
     appBarSpacer: theme.mixins.toolbar,
     content: {
         flexGrow: 1,
-        height: '100vh',
         overflow: 'auto',
     },
-    container: {
-        paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4),
-    },
-    paper: {
+    footer: {
         padding: theme.spacing(2),
-        display: 'flex',
-        overflow: 'auto',
-        flexDirection: 'column',
-    },
-    fixedHeight: {
-        height: 240,
     },
 }));
 
 export default function Dashboard() {
     const classes = useStyles();
-    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
     return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <Navigator/>
-            <main className={classes.content}>
-                <div className={classes.appBarSpacer} />
-                <Container maxWidth="lg" className={classes.container}>
-                    <Grid container spacing={3}>
-                        {/* Chart */}
-                        <Grid item xs={12} md={8} lg={9}>
-                            <Paper className={fixedHeightPaper}>
-                                <Chart />
-                            </Paper>
-                        </Grid>
-                        {/* Recent Deposits */}
-                        <Grid item xs={12} md={4} lg={3}>
-                            <Paper className={fixedHeightPaper}>
-                            </Paper>
-                        </Grid>
-                        {/* Recent Orders */}
-                        <Grid item xs={12}>
-                            <Paper className={classes.paper}>
-                                <Orders />
-                            </Paper>
-                        </Grid>
-                    </Grid>
-                    <Box pt={4}>
-                        <Copyright />
-                    </Box>
-                </Container>
-            </main>
-        </div>
+        <Router>
+            <div className={classes.app}>
+                <CssBaseline/>
+                <Navigator/>
+                <main className={classes.content}>
+                    <div className={classes.appBarSpacer}/>
+                    <Switch>
+                        <Route exact path="/">
+                            <Home/>
+                        </Route>
+                        <Route exact path="/login">
+                            <SingIn/>
+                        </Route>
+                        <Route exact path="/sign-up">
+                            <SingUp/>
+                        </Route>
+                        <Route path="*">
+                            <Error content={"Error 404 Page not found"}/>
+                        </Route>
+                    </Switch>
+                    <footer className={classes.footer}>
+                        <Box pt={4}>
+                            <Copyright/>
+                        </Box>
+                    </footer>
+                </main>
+            </div>
+        </Router>
     );
 }
